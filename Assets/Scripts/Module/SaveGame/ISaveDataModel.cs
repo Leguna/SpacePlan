06b7b31ecc82ef3b810b2bf1c3c47139c241abd7
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2022-08-13 17:03:18 +0800

    Add struct in ISaveDataModel

--- Assets/Scripts/Module/SaveGame/ISaveDataModel.cs
+++ Assets/Scripts/Module/SaveGame/ISaveDataModel.cs
@@ -5,7 +5,7 @@ namespace SpacePlan.Module.SaveGame
     public interface ISaveDataModel : IBaseModel
     {
         // TODO @Icankkk: Delete coin after implement score player
-        public int Coin { get; }
+        private int Coin { get; }
 
         public struct HighScorePlayer
         {
